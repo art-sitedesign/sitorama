ad54f7df3bb22cd1e5867a71f8f9de8206cf93fd
Vladimir Artamonov <viartamonov@avito.ru>
2019-11-25 19:27:40 +0300

    Удаление конфига роутера при удалении проекта

--- app/core/project/remove.go
+++ app/core/project/remove.go
@@ -1,6 +1,12 @@
 package project
 
-import "context"
+import (
+	"context"
+	"os"
+
+	"github.com/art-sitedesign/sitorama/app/core/services"
+	"github.com/art-sitedesign/sitorama/app/utils"
+)
 
 func (p *Project) Remove(ctx context.Context, name string) error {
 	containers, err := p.docker.FindContainers(ctx, name)
@@ -18,6 +24,23 @@ func (p *Project) Remove(ctx context.Context, name string) error {
 		if err != nil {
 			return err
 		}
+	}
+
+	routerConfPath := utils.RouterConfPath(name)
+	err = os.Remove(routerConfPath)
+	if err != nil {
+		return err
+	}
+
+	router := services.NewRouter(p.docker)
+	routerContainer, err := router.Find(ctx)
+	if err != nil {
+		return err
+	}
+
+	err = p.docker.RestartContainer(ctx, routerContainer.ID)
+	if err != nil {
+		return err
 	}
 
 	return nil
